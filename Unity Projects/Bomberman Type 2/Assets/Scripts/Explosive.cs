39763e0f7a30affa5e0c9d7a20afd5fd5a6bdedc
Angelo Goncalves <agoncal7@my.centennialcollege.ca>
2019-10-09 21:13:18 -0400

    Attempting to explode blocks, Failed.

--- Unity Projects/Bomberman Type 2/Assets/Scripts/Explosive.cs
+++ Unity Projects/Bomberman Type 2/Assets/Scripts/Explosive.cs
@@ -4,29 +4,59 @@ using UnityEngine;
 
 public class Explosive : MonoBehaviour
 {
+    public GameObject explosionPrefab;
     public LayerMask levelMask;
     public float countdown = .65f;
 
     void Start()
     {
-
+        StartCoroutine(CreateExplosions(Vector3.forward));
+        StartCoroutine(CreateExplosions(Vector3.right));
+        StartCoroutine(CreateExplosions(Vector3.back));
+        StartCoroutine(CreateExplosions(Vector3.left));
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-
-
-
         countdown -= Time.deltaTime;
 
+        
+        
         if (countdown <= 0f)
         {
             Destroy(gameObject);
         }
+        
 
+
+    }
+    private IEnumerator CreateExplosions(Vector3 direction)
+    {
+        
+            for (int i = 1; i < 3; i++)
+        {
+     
+            RaycastHit hit;
+  
+            Physics.Raycast(transform.position + new Vector3(0, .5f, 0), direction, out hit,
+              i, levelMask);
+
+ 
+            if (!hit.collider)
+            {
+                Instantiate(explosionPrefab, transform.position + (i * direction),
+           
+                  explosionPrefab.transform.rotation);
+             
+            }
+            else
+            {
+                break;
+            }
+            
+            yield return new WaitForSeconds(.05f);
+        }
 
     }
 }
